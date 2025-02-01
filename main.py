56e71c0d239eb8a279f7bbaa6f9f1da57298f271
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-02-01 17:32:55 +0300

    streaming output

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
+import threading
+
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
 
 model = AutoModelForCausalLM.from_pretrained(
@@ -13,6 +15,19 @@ input_text = "Hello, does 2 + 2 equal 5? Give certain answer."
 inputs = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
 input_ids = inputs["input_ids"].to("cuda")
 attention_mask = inputs["attention_mask"].to("cuda")
-outputs = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=10000)
-for output in outputs:
-    print(tokenizer.decode(output, skip_special_tokens=True))
+
+streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+thread = threading.Thread(
+    target=model.generate,
+    kwargs={
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "max_new_tokens": 1000,
+        "streamer": streamer,
+    }
+)
+thread.start()
+
+for token in streamer:
+    print(token, end="", flush=True)
